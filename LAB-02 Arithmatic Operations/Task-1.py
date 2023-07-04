011d566c63dbb4c473e9620a797a32fe4b5ef6b7
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-04 19:22:55 +0500

    Updated

---
+++ LAB-02 Arithmatic Operations/Task-1.py
@@ -0,0 +1,12 @@
+m = int(input("Enter value of m: "))
+n = int(input("Enter value of n: "))
+
+side1 = m**2 - n**2
+side2 = 2 * n * m
+hyp = m**2 + n**2
+
+print("The values of m and n:", m, n)
+print("The sides of Pythagorean Triple are: ")
+print("Side1:", side1)
+print("Side2:", side2)
+print("Hypotenuse:", hyp)
