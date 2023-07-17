2b34eec44ed6dc9ed8cfe6f307881340df5407c7
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-17 09:01:23 +0500

    Updated

--- LAB-09 Functions/Task-2.py
+++ LAB-09 Functions/Task-2.py
@@ -2,8 +2,18 @@ import math
 
 
 def caomputeRadius(c1, c2, p1, p2):
+    c1 = int(c1)
+    c2 = int(c2)
+    p1 = int(p1)
+    p2 = int(p2)
     radius = math.sqrt((c2 - c1) ** 2 + (p2 - p1) ** 2)
     return radius
+
+
+def circle_stats(radius):
+    area = math.pi * radius**2
+    circumference = 2 * math.pi * radius
+    return area, circumference
 
 
 c1 = 0
@@ -12,7 +22,9 @@ c2 = 0
 p1 = 0
 p2 = 0
 
-c1, c2 = tuple(input("Enter center points c1 c2").strip().split())
-p1, p2 = tuple(input("Enter center points p1 p2").strip().split())
+c1, c2 = tuple(input("Enter center points c1 c2: ").strip().split())
+p1, p2 = tuple(input("Enter center points p1 p2: ").strip().split())
 
 radius = caomputeRadius(c1, c2, p1, p2)
+print("Radius is: ", radius)
+print("Area and circumference are: ", circle_stats(radius))
