d04bfafd94a4edf73c62903d1f142559179a07ca
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-22 12:46:56 +0500

    Updated

---
+++ LAB-15 Crossponding Lists/Task-1.py
@@ -0,0 +1,5 @@
+area = ["Lahore", "Karachi", "Gujrat", "Pindi", "Kohat", "Peshawar", "Bannu", "Quetta", "Wahndo", "Pindi"]
+postal = [100, 300, 15, 35, 12, 20, 18, 6, 35, 0]
+
+for i in range(len(area)):
+    print(area[i], postal[i])
