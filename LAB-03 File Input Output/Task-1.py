afcec275e9b92c3677c8a8860fbd98a45e4bd82b
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-22 11:27:49 +0500

    Updated

--- LAB-03 File Input Output/Task-1.py
+++ LAB-03 File Input Output/Task-1.py
@@ -1,4 +1,4 @@
-input_file = open("C:\Users\LENOVO\Repos\PF-Python\LAB-03 File Input Output\input.txt", "r")
+input_file = open("C:\\Users\\LENOVO\\Repos\\PF-Python\\LAB-03 File Input Output\\input.txt", "r")
 input_file_content = input_file.read()
 input_lines = input_file_content.split("\n")
 
@@ -6,9 +6,7 @@ output_lines = []
 
 for emp in input_lines:
     emp_details = emp.split()
-    increased_salary = str(
-        int(emp_details[2]) + (int(emp_details[2]) * int(emp_details[3]) / 100)
-    )
+    increased_salary = str(int(emp_details[2]) + (int(emp_details[2]) * int(emp_details[3]) / 100))
     emp_details_updated = [emp_details[0], emp_details[1], increased_salary]
 
     output_lines.append(" ".join(emp_details_updated))
